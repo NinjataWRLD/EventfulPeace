b38b027a24f287d414454140120f81b32fca05e1
Ivan Angelov <ivanangelov414@gmail.com>
2025-03-22 12:04:10 +0200

    Added S3 Storage Service

---
+++ EventfulPeace.Web/Storage/AmazonS3Service.cs
@@ -0,0 +1,81 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using CustomCADs.Shared.Infrastructure.Storage;
+using EventfulPeace.Application.Common;
+using Microsoft.Extensions.Options;
+
+namespace EventfulPeace.Web.Storage;
+
+public sealed class AmazonS3Service(IAmazonS3 s3Client, IOptions<StorageSettings> settings) : IStorageService
+{
+    public async Task<string> GetPresignedGetUrlAsync(string key, string contentType)
+    {
+        try
+        {
+            GetPreSignedUrlRequest req = new()
+            {
+                BucketName = settings.Value.BucketName,
+                Key = key,
+                Verb = HttpVerb.GET,
+                Expires = DateTime.UtcNow.AddMinutes(2),
+                ContentType = contentType,
+            };
+            return await s3Client.GetPreSignedURLAsync(req).ConfigureAwait(false);
+        }
+        catch (Exception)
+        {
+            throw new($"Retrieving file: {key} went wrong.");
+        }
+    }
+
+    public async Task<(string Key, string Url)> GetPresignedPostUrlAsync(string folderPath, string name, string contentType, string fileName)
+    {
+        try
+        {
+            string extension = fileName[fileName.LastIndexOf('.')..];
+            string key = $"{folderPath}/{name}{Guid.NewGuid()}{extension}";
+
+            GetPreSignedUrlRequest req = new()
+            {
+                BucketName = settings.Value.BucketName,
+                Key = key,
+                Verb = HttpVerb.PUT,
+                Expires = DateTime.UtcNow.AddMinutes(2),
+                ContentType = contentType,
+                Metadata = { ["file-name"] = fileName }
+            };
+
+            string url = await s3Client.GetPreSignedURLAsync(req).ConfigureAwait(false);
+            return (Key: key, Url: url);
+        }
+        catch (AmazonS3Exception ex)
+        {
+            throw new(ex.Message);
+        }
+        catch (Exception)
+        {
+            throw new($"Uploading file: {fileName} went wrong.");
+        }
+    }
+
+    public async Task DeleteFileAsync(string key, CancellationToken ct = default)
+    {
+        try
+        {
+            DeleteObjectRequest req = new()
+            {
+                BucketName = settings.Value.BucketName,
+                Key = key,
+            };
+            await s3Client.DeleteObjectAsync(req, ct).ConfigureAwait(false);
+        }
+        catch (AmazonS3Exception ex)
+        {
+            throw new(ex.Message);
+        }
+        catch (Exception)
+        {
+            throw new($"Deleting file: {key} went wrong.");
+        }
+    }
+}
---
+++ EventfulPeace.Web/Storage/StorageSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace CustomCADs.Shared.Infrastructure.Storage;
+
+public record StorageSettings(string AccessKey, string SecretKey, string Region, string BucketName)
+{
+    public StorageSettings() : this(string.Empty, string.Empty, string.Empty, string.Empty) { }
+}
